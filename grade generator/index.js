9ebcfb08a6f205e8484d85adb3a99d5fd888edaa
jahfar <jahfarmohammed67@gmail.com>
2024-03-25 15:23:47 +0300

    completed assignment

---
+++ grade generator/index.js
@@ -0,0 +1,18 @@
+//User to input marks
+let marks = 45;
+
+//conditional statement to evaluate marks .
+if (marks <= 100 && marks >= 79) {
+  console.log("A");
+} else if (marks >= 60 && marks < 79) {
+  console.log("B");
+}
+if (marks >= 49 && marks < 59) {
+  console.log("C");
+}
+if (marks >= 40 && marks < 49) {
+  console.log("D");
+}
+if (marks >= 0 && marks < 40) {
+  console.log("E");
+}
